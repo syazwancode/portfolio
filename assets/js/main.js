2ab39c243fe7fb5cee00149be0447d7334496fe8
syazwancode <66244514+syazwancode@users.noreply.github.com>
2020-06-25 23:46:42 +0800

    add secret light switch and new picture for light mode and insert href at main logo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 var menu = document.getElementById('menu-mobile-nav')
 var suis = false
+var color = 'dark'
 
 function showMenu() {
 
@@ -24,4 +25,28 @@ function closeMenu() {
     suis = false
     console.log('water! ', suis)
     return null
+}
+
+function colorSwitch() {
+    alert('You found a secret switch!')
+
+    if (color === 'dark') {
+        document.documentElement.style.setProperty("--light", "#1a1a1d");
+        document.documentElement.style.setProperty("--dark", "white");
+        document.documentElement.style.setProperty("--dark-light", "#e3e3e3");
+        document.getElementById('banner-img').style.backgroundImage = "url('assets/img/img-banner-3-light.PNG')"
+        color = 'light'
+        window.scrollTo(0, 0)
+        return false
+    }
+
+    if (color === 'light') {
+        document.documentElement.style.setProperty("--light", "#edf2f4");
+        document.documentElement.style.setProperty("--dark", "#2a2c41");
+        document.documentElement.style.setProperty("--dark-light", "#323653");
+        document.getElementById('banner-img').style.backgroundImage = "url('assets/img/img-banner-3.PNG')"
+        color = 'dark'
+        window.scrollTo(0, 0)
+        return false
+    }
 }
